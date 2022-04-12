f06cd1260e966995830975edfef7e39e67135d48
HuyNguyen19989 <redwoodridofu@gmail.com>
2022-04-12 10:59:36 +0700

    remove unused variables

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import style from './style.module.scss'
 import Modal from './components/modal/modal'
